c710e315a5bc957865933dd055e8d0d86f69b97f
MatthewBMitchell <mitche61@wwu.edu>
2023-02-16 16:34:18 -0800

    nothing

---
+++ src/com/company/Encryption.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Encryption {
+    /*
+        way to generate key
+
+        way to decrypt database
+
+        way to encrypt database
+     */
+
+    /*
+        private key
+
+        Public void passInfo(user.name, user.password)
+            key = makeMasterKey(user.name, user.password)
+
+        private string makeMasterKey(user.name, user.password)
+            return user.name ^ user.password
+
+        public decrytp(data)
+            use 
+     */
+}
